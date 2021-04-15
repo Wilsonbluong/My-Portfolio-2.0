d378fbd0828dff60bd905c0789f1dbfe91638aae
Wilson Luong <wilsonbluong@gmail.com>
2021-04-15 11:45:27 -0700

    updated contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 //import axios from "axios";
 import {
   ContactContainer,
@@ -13,48 +13,7 @@ import {
   //StyledResult,
 } from "./styles/Contact.js";
 
-const onSubmit = async (event, setSubmitText) => {
-  event.preventDefault();
-  setSubmitText("Submitting ...");
-  const formElements = [...event.currentTarget.elements];
-  const isValid =
-    formElements.filter((elem) => elem.name === "bot-field")[0].value === "";
-
-  const validFormElements = isValid ? formElements : [];
-
-  if (validFormElements.length < 1) {
-    // or some other cheeky error message
-    setSubmitText("It looks like you filled out too many fields!");
-  } else {
-    const filledOutElements = validFormElements
-      .filter((elem) => !!elem.value)
-      .map(
-        (element) =>
-          encodeURIComponent(element.name) +
-          "=" +
-          encodeURIComponent(element.value)
-      )
-      .join("&");
-
-    await fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: filledOutElements,
-    })
-      .then(() => {
-        setSubmitText("Successfully submitted!");
-      })
-      .catch((_) => {
-        setSubmitText(
-          "There was an error with your submission, please email me using the address above."
-        );
-      });
-  }
-};
-
 const Contact = () => {
-  const [submitText, setSubmitText] = useState(null);
-
   return (
     <>
       <ContactContainer id="contact">
@@ -67,7 +26,7 @@ const Contact = () => {
             name="contact-wilson"
             method="POST"
             data-netlify="true"
-            onSubmit={(e) => onSubmit(e, setSubmitText)}
+            onSubmit="submit"
           >
             <input type="hidden" name="form-name" value="contact v1" />
             <StyledInput
@@ -94,7 +53,6 @@ const Contact = () => {
               Submit
             </StyledButton>
           </StyledForm>
-          {submitText && <p>{submitText}</p>}
         </FormWrapper>
       </ContactContainer>
     </>
